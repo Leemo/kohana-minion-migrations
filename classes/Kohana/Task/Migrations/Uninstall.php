c280cc439be6704c5bd3319011942d066564937d
Alexey Popov <6425762@gmail.com>
2013-11-27 04:59:46 +0000

    A bug with install/uninstall fixed

--- classes/Kohana/Task/Migrations/Uninstall.php
+++ classes/Kohana/Task/Migrations/Uninstall.php
@@ -41,8 +41,8 @@ class Kohana_Task_Migrations_Uninstall extends Minion_Task {
 
 		try
 		{
-			DB::query(NULL, str_replace(':prefix_', $table_prefix, $query))
-				->execute();
+			Database::instance()
+				->query(NULL, str_replace(':prefix_', $table_prefix, $query));
 		}
 		catch (Exception $e)
 		{
